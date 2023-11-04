c1ce3fc3823e2143eed9a40856d9ee70ca3e0888
aew apisit <aew.apisit@gmail.com>
2023-11-04 20:45:17 +0700

    first commit

--- Hello.py
+++ Hello.py
@@ -13,10 +13,18 @@
 # limitations under the License.
 
 import streamlit as st
+import pandas as pd
 from streamlit.logger import get_logger
+
+import psycopg2
+import datetime
+import time
+
 
 LOGGER = get_logger(__name__)
 
+def convert_df(df):
+      return df.to_csv(index=False).encode('utf-8')
 
 def run():
     st.set_page_config(
@@ -28,24 +36,50 @@ def run():
 
     st.sidebar.success("Select a demo above.")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    today = datetime.datetime.now()
+    next_year = today.year
+    jan_1 = datetime.date(next_year, 1, 1)
+    dec_31 = datetime.date(next_year, 12, 31)
+    
 
+    noww = datetime.date(today.year, today.month, today.day)
+    # st.write(type(jan_1))
+    
+    
+    
+    d = st.date_input(
+        "Select your vacation for next year",
+        (noww, noww),
+        jan_1,
+        dec_31,
+        format="MM.DD.YYYY",
+    )
+    
+    if len(d) == 2:
+      unixtime0 = time.mktime(d[0].timetuple())
+      unixtime1 = time.mktime(d[1].timetuple())
+
+      # Initialize connection.
+      conn = st.connection("postgresql", type="sql")
+      df = conn.query(f'SELECT * FROM mobiles WHERE created_at > {unixtime0} AND created_at < {unixtime1};', ttl="10m")
+      with st.expander("See explanation Data"):
+        st.write(df)
+      
+      
+
+      df['time_stamp'] = pd.to_datetime(df['created_at'],unit='s')
+      df1 = df[['time_stamp', 'temp', 'unit_temp', 'sku', 'area', 'plant']]
+      st.write(df1)
+      csv = convert_df(df1)
+
+
+      st.download_button(
+        "Press to Download",
+        csv,
+        "file.csv",
+        "text/csv",
+        key='download-csv'
+      )
 
 if __name__ == "__main__":
     run()
